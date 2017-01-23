71618a40318f96005fc0a8e798374ed9bdd750df
Christopher Haster <chaster@utexas.edu>
2017-01-23 07:06:44 -0600

    Added the ReadStream and WriteStream interfaces
    ReadStream - Interface for reading from streams of bytes
    WriteStream - Interface for writing to streams of bytes

---
+++ platform/ReadStream.h
@@ -0,0 +1,82 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2013 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MBED_READ_STREAM_H
+#define MBED_READ_STREAM_H
+
+#include "platform/platform.h"
+#include <cstdarg>
+
+namespace mbed {
+/** \addtogroup platform */
+/** @{*/
+
+
+/** ReadStream - Interface for reading from streams of bytes
+ */
+class ReadStream {
+public:
+    /** ReadStream lifetime
+     */
+    virtual ~ReadStream() {}
+
+    /** Read data from the underlying stream
+     *
+     *  @param buffer   Destination buffer for read data
+     *  @param size     Size of buffer in bytes
+     *  @return         Number of read bytes, negative value on failure
+     */
+    virtual ssize_t read(void *buffer, size_t size) = 0;
+
+    /** Read data from the underlying stream until size has been read
+     *
+     *  @param buffer   Destination buffer for read data
+     *  @param size     Size of buffer in bytes
+     *  @return         Number of read bytes, negative value on failure
+     */
+    ssize_t readall(void *buffer, size_t size);
+
+    /** Get a character from the underlying stream
+     *
+     *  This is equivalent to read with size of 1
+     *
+     *  @return         8-bit character read, negative value on failure
+     */
+    int getc();
+
+    /** Read formatted input from the underlying stream
+     *
+     *  @param format   Format string to scanf
+     *  @param ...      Arguments to scanf
+     *  @return         Number of patterns matched, negative value on failure
+     *  @see scanf
+     */
+    ssize_t scanf(const char *format, ...);
+
+    /** Read formatted input from the underlying stream
+     *
+     *  @param format   Format string to scanf
+     *  @param args     Arguments to scanf
+     *  @return         Number of patterns matched, negative value on failure
+     *  @see scanf
+     */
+    ssize_t vscanf(const char *format, va_list args);
+};
+
+
+/** @}*/
+} // namespace mbed
+
+#endif
---
+++ platform/WriteStream.h
@@ -0,0 +1,83 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2013 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MBED_WRITE_STREAM_H
+#define MBED_WRITE_STREAM_H
+
+#include "platform/platform.h"
+#include <cstdarg>
+
+namespace mbed {
+/** \addtogroup platform */
+/** @{*/
+
+
+/** WriteStream - Interface for writing to streams of bytes
+ */
+class WriteStream {
+public:
+    /** WriteStream lifetime
+     */
+    virtual ~WriteStream() {}
+
+    /** Write data to the underlying stream
+     *
+     *  @param buffer   Buffer of data to write
+     *  @param size     Size of buffer in bytes
+     *  @return         Number of written bytes, negative value on failure
+     */
+    virtual ssize_t write(const void *buffer, size_t size) = 0;
+
+    /** Write data to the underlying stream until all data has been written
+     *
+     *  @param buffer   Buffer of data to write
+     *  @param size     Size of buffer in bytes
+     *  @return         Number of written bytes, negative value on failure
+     */
+    ssize_t writeall(const void *buffer, size_t size);
+
+    /** Write a character from the underlying stream
+     *
+     *  This is equivalent to write with size of 1
+     *
+     *  @param c        8-bit character to write
+     *  @return         The written character on success, negative value on failure
+     */
+    int putc(int c);
+
+    /** Write formatted output to the underlying stream
+     *
+     *  @param format   Format string to printf
+     *  @param ...      Arguments to printf
+     *  @return         Number of bytes written, negative value on failure
+     *  @see printf
+     */
+    ssize_t printf(const char *format, ...);
+
+    /** Write formatted output to the underlying stream
+     *
+     *  @param format   Format string to printf
+     *  @param args     Arguments to printf
+     *  @return         Number of bytes written, negative value on failure
+     *  @see printf
+     */
+    ssize_t vprintf(const char *format, va_list args);
+};
+
+
+/** @}*/
+} // namespace mbed
+
+#endif
